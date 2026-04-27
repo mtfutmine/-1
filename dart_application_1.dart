9d648aa338a67de37adba3ccb28d6c9df0327f90
mtfutmine <mtfutmine@gmail.com>
2026-04-27 23:33:47 +0300

    Add files via upload

--- dart_application_1.dart
+++ dart_application_1.dart
@@ -1,132 +1,255 @@
+// 1. Кружка и Человек
+class Cup {
+  int volume = 100; // мл
+  int current = 100; // текущее количество
+
+  void drink(int amount) {
+    if (amount <= current) {
+      current -= amount;
+      print('Выпито $amount мл. Осталось $current мл.');
+    } else {
+      print('В кружке недостаточно воды.');
+    }
+  }
+}
+
+class Human {
+  void drinkFromCup(Cup cup, int amount) {
+    cup.drink(amount);
+  }
+}
+
+// 2. Шкаф с системами хранения
+class StorageSystem {
+  List<String> items = [];
+
+  void put(String item) => items.add(item);
+  String? take(String item) {
+    if (items.remove(item)) return item;
+    return null;
+  }
+}
+
+class Cupboard {
+  List<StorageSystem> storages = [];
+
+  void addStorage(StorageSystem storage) => storages.add(storage);
+  void putItem(String item, int storageIndex) {
+    if (storageIndex < storages.length) {
+      storages[storageIndex].put(item);
+    }
+  }
+  String? takeItem(String item, int storageIndex) {
+    if (storageIndex < storages.length) {
+      return storages[storageIndex].take(item);
+    }
+    return null;
+  }
+}
+
+// 3. Гриф и Блин
+class Plate {
+  double weight;
+  Plate(this.weight);
+}
+
+class Barbell {
+  double maxLoad;
+  List<Plate> leftPlates = [];
+  List<Plate> rightPlates = [];
+
+  Barbell(this.maxLoad);
+
+  double get currentLoad {
+    double sum = 0;
+    for (var p in leftPlates) sum += p.weight;
+    for (var p in rightPlates) sum += p.weight;
+    return sum;
+  }
+
+  bool canAdd(Plate plate) => currentLoad + plate.weight <= maxLoad;
+
+  void addLeft(Plate plate) {
+    if (canAdd(plate)) leftPlates.add(plate);
+    else print('Превышение нагрузки!');
+  }
+
+  void addRight(Plate plate) {
+    if (canAdd(plate)) rightPlates.add(plate);
+    else print('Превышение нагрузки!');
+  }
+}
+
+// 4. Конвертация валют
+class CurrencyConverter {
+  static double convert(double amount, String from, String to) {
+    // упрощённые курсы
+    Map<String, double> rates = {
+      'USD': 1.0,
+      'EUR': 0.85,
+      'RUB': 75.0,
+    };
+    if (!rates.containsKey(from) || !rates.containsKey(to)) return amount;
+    double inUSD = amount / rates[from]!;
+    return inUSD * rates[to]!;
+  }
+}
+
+// 5. Гараж с дженериками
+class Garage<T> {
+  List<T> items = [];
+  void store(T item) => items.add(item);
+  T? retrieve(int index) => index < items.length ? items[index] : null;
+}
+
+// 6. Класс с перегрузкой арифметических операций
+class Vector2 {
+  double x, y;
+  Vector2(this.x, this.y);
+
+  Vector2 operator +(Vector2 other) => Vector2(x + other.x, y + other.y);
+  Vector2 operator -(Vector2 other) => Vector2(x - other.x, y - other.y);
+  Vector2 operator *(double scalar) => Vector2(x * scalar, y * scalar);
+  Vector2 operator /(double scalar) => Vector2(x / scalar, y / scalar);
+  bool operator ==(Object other) =>
+      other is Vector2 && x == other.x && y == other.y;
+
+  @override
+  String toString() => 'Vector2($x, $y)';
+}
+
+// 7. Автомобиль с перечислениями
+enum CarState { stop, moving, turning }
+
+class Car {
+  CarState state = CarState.stop;
+  void drive() => state = CarState.moving;
+  void turn() => state = CarState.turning;
+  void stop() => state = CarState.stop;
+  void showState() => print('Состояние: $state');
+}
+
+// 8. Геометрические фигуры
+abstract class Shape {
+  double area();
+}
+
+class Rectangle extends Shape {
+  double width, height;
+  Rectangle(this.width, this.height);
+  @override
+  double area() => width * height;
+}
+
+class Triangle extends Shape {
+  double base, height;
+  Triangle(this.base, this.height);
+  @override
+  double area() => 0.5 * base * height;
+}
+
+class Circle extends Shape {
+  double radius;
+  Circle(this.radius);
+  @override
+  double area() => 3.14159 * radius * radius;
+}
+
+// 9. Конвертер систем счисления
+class NumberBaseConverter {
+  static String fromDecimal(int number, int base) {
+    if (base == 16) return number.toRadixString(16).toUpperCase();
+    if (base == 8) return number.toRadixString(8);
+    if (base == 10) return number.toString();
+    return 'Unsupported base';
+  }
+
+  static int toDecimal(String value, int base) {
+    return int.parse(value, radix: base);
+  }
+}
+
+// 10. Список фигур с поиском максимальной площади
+class ShapeCollection {
+  List<Shape> shapes = [];
+  void add(Shape shape) => shapes.add(shape);
+  Shape? getMaxAreaShape() {
+    if (shapes.isEmpty) return null;
+    return shapes.reduce((a, b) => a.area() > b.area() ? a : b);
+  }
+}
+
+// 11. Стол и столовые приборы
+abstract class Cutlery {
+  String name;
+  Cutlery(this.name);
+}
+
+class Fork extends Cutlery { Fork() : super('Вилка'); }
+class Spoon extends Cutlery { Spoon() : super('Ложка'); }
+class Knife extends Cutlery { Knife() : super('Нож'); }
+
+class Table {
+  List<Cutlery> items = [];
+  void put(Cutlery item) => items.add(item);
+  bool remove(Cutlery item) => items.remove(item);
+  void showItems() => print('На столе: ${items.map((c) => c.name).join(', ')}');
+}
+
+// Пример использования (можно закомментировать)
 void main() {
-  List<String> students = ['Владислав', 'Аким', 'Иван', 'Алевдин', 'Никита', 'Павел'];
-  List<String> subjects = ['ОАИП', 'ОСИС', 'РПМ', 'РМП'];
+  // 1
+  var cup = Cup();
+  var human = Human();
+  human.drinkFromCup(cup, 30);
 
-  Map<String, Map<String, int>> grades = {
-    'Владислав': {'ОАИП': 5, 'ОСИС': 4, 'РПМ': 5, 'РМП': 4},
-    'Аким': {'ОАИП': 3, 'ОСИС': 4, 'РПМ': 3, 'РМП': 5},
-    'Иван': {'ОАИП': 4, 'ОСИС': 5, 'РПМ': 4, 'РМП': 4},
-    'Алевдин': {'ОАИП': 5, 'ОСИС': 5, 'РПМ': 5, 'РМП': 5},
-    'Никита': {'ОАИП': 2, 'ОСИС': 3, 'РПМ': 4, 'РМП': 3},
-    'Павел': {'ОАИП': 4, 'ОСИС': 4, 'РПМ': 4, 'РМП': 4},
-  };
+  // 2
+  var cupboard = Cupboard();
+  cupboard.addStorage(StorageSystem());
+  cupboard.putItem('Книга', 0);
+  print(cupboard.takeItem('Книга', 0));
 
-  for (int i = 0; i < students.length; i++) {
-    print('${i + 1}. ${students[i]}');
-  }
-  print('');
+  // 3
+  var barbell = Barbell(100);
+  barbell.addLeft(Plate(20));
+  barbell.addRight(Plate(20));
+  print('Текущий вес: ${barbell.currentLoad}');
 
-  for (int i = 0; i < subjects.length; i++) {
-    print('${i + 1}. ${subjects[i]}');
-  }
-  print('');
+  // 4
+  print(CurrencyConverter.convert(100, 'USD', 'EUR'));
 
-  for (var student in students) {
-    var studentGrades = grades[student]!;
-    for (var subject in subjects) {
-      print('$student — $subject: ${studentGrades[subject]}');
-    }
-  }
-  print('');
+  // 5
+  var garage = Garage<String>();
+  garage.store('Велосипед');
+  print(garage.retrieve(0));
 
-  for (var subject in subjects) {
-    double sum = 0;
-    for (var student in students) {
-      sum += grades[student]![subject]!;
-    }
-    double avg = sum / students.length;
-    print('$subject: ${avg.toStringAsFixed(2)}');
-  }
-  print('');
+  // 6
+  var v1 = Vector2(1, 2);
+  var v2 = Vector2(3, 4);
+  print(v1 + v2);
 
-  for (var student in students) {
-    double sum = 0;
-    var studentGrades = grades[student]!;
-    for (var subject in subjects) {
-      sum += studentGrades[subject]!;
-    }
-    double avg = sum / subjects.length;
-    print('$student: ${avg.toStringAsFixed(2)}');
-  }
-  print('');
+  // 7
+  var car = Car();
+  car.drive();
+  car.showState();
 
-  String bestStudent = '';
-  double bestAvg = 0;
-  for (var student in students) {
-    double sum = 0;
-    for (var subject in subjects) {
-      sum += grades[student]![subject]!;
-    }
-    double avg = sum / subjects.length;
-    if (avg > bestAvg) {
-      bestAvg = avg;
-      bestStudent = student;
-    }
-  }
-  print('$bestStudent ${bestAvg.toStringAsFixed(2)}');
-  print('');
+  // 8
+  var rect = Rectangle(5, 10);
+  print('Площадь прямоугольника: ${rect.area()}');
 
-  String worstSubject = '';
-  double worstAvg = double.infinity;
-  for (var subject in subjects) {
-    double sum = 0;
-    for (var student in students) {
-      sum += grades[student]![subject]!;
-    }
-    double avg = sum / students.length;
-    if (avg < worstAvg) {
-      worstAvg = avg;
-      worstSubject = subject;
-    }
-  }
-  print('$worstSubject ${worstAvg.toStringAsFixed(2)}');
-  print('');
+  // 9
+  print(NumberBaseConverter.fromDecimal(255, 16)); // FF
+  print(NumberBaseConverter.toDecimal('FF', 16)); // 255
 
-  double totalSum = 0;
-  int totalCount = 0;
-  for (var student in students) {
-    for (var subject in subjects) {
-      totalSum += grades[student]![subject]!;
-      totalCount++;
-    }
-  }
-  double overallAvg = totalSum / totalCount;
-  print(overallAvg.toStringAsFixed(2));
-  print('');
+  // 10
+  var coll = ShapeCollection();
+  coll.add(Circle(5));
+  coll.add(Rectangle(4, 6));
+  print('Макс площадь: ${coll.getMaxAreaShape()?.area()}');
 
-  print('${subjects.length} $subjects');
-  print('');
-
-  List<String> noTwos = [];
-  for (var student in students) {
-    bool hasTwo = false;
-    var studentGrades = grades[student]!;
-    for (var subject in subjects) {
-      if (studentGrades[subject] == 2) {
-        hasTwo = true;
-        break;
-      }
-    }
-    if (!hasTwo) {
-      noTwos.add(student);
-    }
-  }
-  print(noTwos.isEmpty ? 'нет' : noTwos.join(', '));
-  print('');
-
-  List<String> allFourPlus = [];
-  for (var student in students) {
-    bool allOk = true;
-    var studentGrades = grades[student]!;
-    for (var subject in subjects) {
-      if (studentGrades[subject]! < 4) {
-        allOk = false;
-        break;
-      }
-    }
-    if (allOk) {
-      allFourPlus.add(student);
-    }
-  }
-  print(allFourPlus.isEmpty ? 'нет' : allFourPlus.join(', '));
+  // 11
+  var table = Table();
+  table.put(Fork());
+  table.put(Spoon());
+  table.showItems();
 }
